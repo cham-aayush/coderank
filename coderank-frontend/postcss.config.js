35bde0b2527d4ca2ddbee40961ac31af35da1cde
cham-aayush <aayushchamoli2020@gmail.com>
2025-07-12 17:06:18 +0530

    Changes made in Tailwind config and postcss config

--- coderank-frontend/postcss.config.js
+++ coderank-frontend/postcss.config.js
@@ -1,16 +1,6 @@
 module.exports = {
-  darkMode: 'class',
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        retro: ['"VT323"', 'monospace']
-      },
-      colors: {
-        retroGreen: '#00FF41',
-        retroBg: '#0d0d0d'
-      }
-    }
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [],
 };
--- coderank-frontend/tailwind.config.js
+++ coderank-frontend/tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   darkMode: 'class',
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        retro: ['"VT323"', 'monospace']
+      },
+      colors: {
+        retroGreen: '#00FF41',
+        retroBg: '#0d0d0d'
+      }
+    }
   },
   plugins: [],
 };
