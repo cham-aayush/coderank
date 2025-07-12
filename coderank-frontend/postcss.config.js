58e6b89df692b7f7f1f621d89b95fbaafc150dc0
cham-aayush <120579452+cham-aayush@users.noreply.github.com>
2025-07-12 17:01:08 +0530

    Update postcss.config.js

--- coderank-frontend/postcss.config.js
+++ coderank-frontend/postcss.config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  darkMode: 'class',
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        retro: ['"VT323"', 'monospace']
+      },
+      colors: {
+        retroGreen: '#00FF41',
+        retroBg: '#0d0d0d'
+      }
+    }
   },
+  plugins: [],
 };
